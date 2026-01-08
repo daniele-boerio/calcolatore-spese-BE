c5233f57b67d3a549903f37fb0467ecacaf5ee31
Silvio <saccomansilvio@gmail.com>
2026-01-08 14:01:18 +0100

    aggiunta di un blocco per eseguire l'applicazione con uvicorn

--- main.py
+++ main.py
@@ -414,3 +414,7 @@ def delete_investimento(
     db.delete(db_investimento)
     db.commit()
     return None
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
