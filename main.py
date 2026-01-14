21ff4e6bfda5e708d46bd50ae029594c471ec9bb
Daniele Boerio <boerio99@gmail.com>
2026-01-14 18:28:25 +0100

    aggiunta di un blocco per eseguire uvicorn da locale

--- main.py
+++ main.py
@@ -45,6 +45,6 @@ def read_root():
     return {"message": "Backend attivo e modulare!"}
 
 
-
-
-
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
